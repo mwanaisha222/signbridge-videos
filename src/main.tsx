78ce00190a1e41e4e6996c592f78d31074777c6e
Marcollins2 <collinslusiba@gmail.com>
2026-05-01 09:35:27 +0300

    Add SPA redirect rules

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { SignBridgeApp } from '@/components/SignBridgeApp'
-import '../styles.css'
+import './styles.css'
 
 const container = document.getElementById('root')
 if (!container) {
